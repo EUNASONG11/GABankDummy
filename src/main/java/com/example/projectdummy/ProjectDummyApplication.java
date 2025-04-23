b31b80c493f43456aca025c1f8f14be626210ede
user <hn04223@gmail.com>
2025-04-23 13:07:50 +0900

    기본틀

---
+++ src/main/java/com/example/projectdummy/ProjectDummyApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectdummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDummyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDummyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectdummy/ProjectDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectdummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDummyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
