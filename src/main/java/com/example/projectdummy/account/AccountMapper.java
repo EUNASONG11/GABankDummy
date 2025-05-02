b3a798e5f795d7e26c532bc7393f88fa482b51dc
ungzzang <godnd1998@gmail.com>
2025-05-02 13:13:24 +0900

    계좌더미하는중

--- src/main/java/com/example/projectdummy/account/AccountMapper.java
+++ src/main/java/com/example/projectdummy/account/AccountMapper.java
@@ -1,7 +1,11 @@
 package com.example.projectdummy.account;
 
+import com.example.projectdummy.account.model.BankAccount;
+import com.example.projectdummy.customer.model.Customer;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AccountMapper {
+    int insBankAccount(BankAccount bankAccount);
+    int updBankAccount(BankAccount bankAccount);
 }
