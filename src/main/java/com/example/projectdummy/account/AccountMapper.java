2e8775d92765df9354518d888d261c23703ba10d
user <ksj23444@gmail.com>
2025-05-07 11:05:15 +0900

    계좌 더미 생성. 아직 상품 더미가 완료되지 않아서 테스트는 안해봄

--- src/main/java/com/example/projectdummy/account/AccountMapper.java
+++ src/main/java/com/example/projectdummy/account/AccountMapper.java
@@ -1,7 +1,17 @@
 package com.example.projectdummy.account;
 
+import com.example.projectdummy.account.model.BankAccount;
+import com.example.projectdummy.productAndDeposit.Product;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface AccountMapper {
+    List<Product> findProduct();
+    List<Long> findCustomer();
+    List<Long> findEmployee();
+
+    int saveBankAccount(BankAccount bankAccount);
+
 }
--- src/main/java/com/example/projectdummy/account/model/BankAccount.java
+++ src/main/java/com/example/projectdummy/account/model/BankAccount.java
@@ -1,6 +1,8 @@
 package com.example.projectdummy.account.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
