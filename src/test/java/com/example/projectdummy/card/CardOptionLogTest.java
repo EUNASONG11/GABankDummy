b17aeb12eb7b0052827ec73684a56a085f224648
user <hn04223@gmail.com>
2025-05-08 11:25:22 +0900

    카드옵션변경

---
+++ src/test/java/com/example/projectdummy/card/CardOptionLogTest.java
@@ -0,0 +1,19 @@
+package com.example.projectdummy.card;
+
+import com.example.projectdummy.DummyDefault;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardOptionLogTest extends DummyDefault {
+
+    @Autowired
+    CardMapper cardMapper;
+
+    @Test
+    void optionLogTest() {
+
+    }
+
+}
