3e9f02885f98148b54d767f76c8ef00afc8c3e8c
boyunsim <tlaqhdbs12@gmail.com>
2025-05-13 12:59:34 +0900

    5/13

--- src/test/java/com/example/projectdummy/loan/LoanRepaymentDummy.java
+++ src/test/java/com/example/projectdummy/loan/LoanRepaymentDummy.java
@@ -86,7 +86,7 @@ public class LoanRepaymentDummy extends DummyDefault {
                     if (j == totalMonths && month.isBefore(LocalDate.now())) {
                         loanRepayment.setFinalAt(month);
                     }
-                    if (month.isBefore(createdAt)) {
+                    if (month.isBefore(LocalDate.now())) {
                         loanRepayment.setDueCode("01202");
                     } else {
                         loanRepayment.setDueCode("01201");
